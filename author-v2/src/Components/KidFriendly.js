94cbe03aaf5fb5b6151197d776b9653103c38b40
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-01-13 11:38:47 -0500

    Update KidFriendly.js

--- author-v2/src/Components/KidFriendly.js
+++ author-v2/src/Components/KidFriendly.js
@@ -6,6 +6,7 @@ import Copyright from './Copyright';
 function KidFriendly() {
   return (
     <>
+    {/* TODO: create write-up and review section for kids book */}
         <Header />
         <h2>Kid Friendly Book</h2>
         <p>include The True Tale of Peter Piper with reviews, snippet, picture, and links to purchase</p> 
