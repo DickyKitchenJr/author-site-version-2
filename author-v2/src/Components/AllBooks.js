84999564a4541bc11cea29b9b674596667c27fc1
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-01-13 11:41:13 -0500

    Update AllBooks.js

--- author-v2/src/Components/AllBooks.js
+++ author-v2/src/Components/AllBooks.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './AllBooks.css';
-import Header from './Header';
-import Copyright from './Copyright';
+import React from "react";
+import "./AllBooks.css";
+import Header from "./Header";
+import Copyright from "./Copyright";
 
 function AllBooks() {
   return (
     <>
-    {/* TODO: create write-ups for book blurbs and reviews, possibly use the ones from the previous site? */}
-        <Header />
-        <h2>All Books</h2>
-        <p>include all books, pictures, snippets, reviews, and links</p>
-        <Copyright /> 
+      {/* TODO: create write-ups for book blurbs and reviews, possibly use the ones from the previous site? Possibly use a click through option for the reviews to save on screen space? */}
+      <Header />
+      <h2>All Books</h2>
+      <p>include all books, pictures, snippets, reviews, and links</p>
+      <Copyright />
     </>
-  )
+  );
 }
 
-export default AllBooks
+export default AllBooks;
