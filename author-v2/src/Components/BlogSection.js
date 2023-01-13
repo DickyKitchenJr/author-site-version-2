9f05e9cd1378c0aba3ee136af6e299481d4b61c0
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-01-13 11:36:56 -0500

    Update BlogSection.js

--- author-v2/src/Components/BlogSection.js
+++ author-v2/src/Components/BlogSection.js
@@ -6,6 +6,7 @@ import Copyright from "./Copyright";
 function BlogSection() {
   return (
     <>
+    {/* TODO: Create addition files holding blogs; create links for individual blogs, look into building a comments section */}
       <Header />
       <h2>Blogs (Welcome to My Mind /The Unusually Named Writer)</h2>
       <p>nav section leading to blogs, maybe a visual. Include comment section in the blogs?</p>
