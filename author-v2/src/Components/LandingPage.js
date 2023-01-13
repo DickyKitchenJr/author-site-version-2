190b42a9c300f2876a90b92dc56be5a8c19fe4a1
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-01-13 11:39:57 -0500

    Update LandingPage.js

--- author-v2/src/Components/LandingPage.js
+++ author-v2/src/Components/LandingPage.js
@@ -5,6 +5,7 @@ import Copyright from './Copyright'
 function LandingPage() {
   return (
     <>
+    {/* TODO: add friendly author pic and navigation section, possibly a short intro or hook? */}
      <h1>Landing Page</h1> 
      <p>include navs and brief intro with image?</p>
      <Copyright />
