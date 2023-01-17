f499f174ed7a2667fd1a6f0ca517cc0b602d5878
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-01-17 09:31:08 -0500

    Update Copyright.js

--- author-v2/src/Components/Copyright.js
+++ author-v2/src/Components/Copyright.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./Copyright.css";
 
 function Copyright() {
   return (
     <>
-      <>
-        <p className="copyright">
-          Copyright Notice: All writings, blogs, books, and content within this
-          site belong to Dicky Kitchen Jr and are not permitted to be used
-          without written permission.
-        </p>
-      </>
+      <p className="copyright">
+        Copyright Notice: All writings, blogs, books, and content within this
+        site belong to Dicky Kitchen Jr and are not permitted to be used without
+        written permission.
+      </p>
     </>
   );
 }
 
-export default Copyright
+export default Copyright;
