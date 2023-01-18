393f46bb58d242fd945682635fa03b2cb80578b9
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-01-18 11:14:46 -0500

    Update LandingPage.js

--- author-v2/src/Components/LandingPage.js
+++ author-v2/src/Components/LandingPage.js
@@ -1,16 +1,37 @@
-import React from 'react'
-import './LandingPage.css'
-import Copyright from './Copyright'
+import React from "react";
+import "./LandingPage.css";
+import { Parallax } from "react-parallax";
+import Header from "./Header";
+import Copyright from "./Copyright";
+import OpenBook from "../Images/open-book-angled.jpg";
+import BWAuthor from '../Images/author-no-background.png';
 
 function LandingPage() {
   return (
     <>
-    {/* TODO: add friendly author pic and navigation section, possibly a short intro or hook? */}
-     <h1>Landing Page</h1> 
-     <p>include navs and brief intro with image?</p>
-     <Copyright />
+      {/* TODO: add friendly author pic and navigation section, possibly a short intro or hook? */}
+      <Parallax
+        blur={3}
+        bgImage={OpenBook}
+        bgImageAlt="opened book"
+        strength={500}
+      >
+        <Header />
+
+        <main className="landing-main">
+          <nav className="landing-nav">
+          <a className="landing-link" href='#'>ABOUT</a>
+          <a className="landing-link" href="#">BLOG</a>
+          <a className="landing-link" href="#">THRILLERS</a>
+          <a className="landing-link" href="#">KID FRIENDLY</a>
+        </nav>
+        <img className="bw-author" src={BWAuthor} alt="black and white of the author" />
+        </main>
+        
+        <Copyright />
+      </Parallax>
     </>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
