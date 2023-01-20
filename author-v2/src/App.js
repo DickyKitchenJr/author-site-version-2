c0ee83114325d2e03453c61ec3cb217924bd124b
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-01-20 08:59:13 -0500

    Update App.js

--- author-v2/src/App.js
+++ author-v2/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import LandingPage from './Components/LandingPage';
 import About from './Components/About';
 import KidFriendly from './Components/KidFriendly';
@@ -8,11 +9,13 @@ import BlogSection from './Components/BlogSection';
 function App() {
   return (
     <>
-      <LandingPage />
-      <About />
-      <KidFriendly />
-      <AllBooks />
-      <BlogSection />
+      <Routes>
+        <Route path='/' element = {<LandingPage />} />
+        <Route path='/about' element= {<About />} />
+        <Route path='/blogs' element={<BlogSection />} />
+        <Route path='/thrillers' element={<AllBooks />} />
+        <Route path='/kidfriendly' element= {<KidFriendly />} />
+      </Routes>
     </>
   );
 }
