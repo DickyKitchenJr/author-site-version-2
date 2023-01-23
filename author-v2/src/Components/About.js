c6acaecdcdc6106e4ffca0665bb9bc44b7b937ed
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-01-23 13:42:33 -0500

    Update About.js

--- author-v2/src/Components/About.js
+++ author-v2/src/Components/About.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "./About.css";
-import { Twirl as Hamburger } from 'hamburger-react';
+import { Twirl as Hamburger } from "hamburger-react";
 import Header from "./Header";
 import Nav from "./Nav";
 import Copyright from "./Copyright";
+import TypingAuthor from "../Images/author-typing.webp";
 
 function About() {
   const [isOpen, setOpen] = useState(false);
@@ -12,32 +13,50 @@ function About() {
     <>
       {/* TODO: Finish writing bio and possibly include a links section for resources */}
       <Header />
+
       <div className="nav-menu">
         <Hamburger toggled={isOpen} toggle={setOpen} />
         {isOpen ? <Nav /> : null}
       </div>
-      
 
-      <h2>Meet the Author</h2>
-      <p>
-        About the author section with picture and mention of BookInd, possible
-        links to resources I use (editor, image creation, software, etc)
-      </p>
-      <p>
-        What happens when you name your child after a body part and a room in
-        the house?
-        <br />
-        <br />
-        He creates his own serial killer, breaks the rules of writing by
-        creating a story with both third and first person storytelling (all
-        while not giving names to any significant character), and creates an
-        ending that has left readers gasping and in awe! That's what.
-        <br />
-        <br />
-        Dicky Kitchen Jr brings his unique personality and life experiences to
-        his writings (probably shouldn't say that after the whole "creates his
-        own serial killer" thing), resulting in stories unlike any seen before.
-      </p>
+      <main className="about-main">
+        <div className="about-header">
+          <h2 className="about-h2">
+            Meet
+            <br />
+            The
+            <br />
+            Author
+          </h2>
+          <img
+            className="typing-author"
+            src={TypingAuthor}
+            alt="Dicky Kitchen Jr typing on his laptop"
+          />
+        </div>
+
+        <p>
+          About the author section with picture and mention of BookInd, possible
+          links to resources I use (editor, image creation, software, etc)
+        </p>
+        <p>
+          What happens when you name your child after a body part and a room in
+          the house?
+          <br />
+          <br />
+          He creates his own serial killer, breaks the rules of writing by
+          creating a story with both third and first person storytelling (all
+          while not giving names to any significant character), and creates an
+          ending that has left readers gasping and in awe! That's what.
+          <br />
+          <br />
+          Dicky Kitchen Jr brings his unique personality and life experiences to
+          his writings (probably shouldn't say that after the whole "creates his
+          own serial killer" thing), resulting in stories unlike any seen
+          before.
+        </p>
+      </main>
+
       <Copyright />
     </>
   );
