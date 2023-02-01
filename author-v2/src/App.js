dd2e4bda82ad05faf9a00eee49604c0cb987cd08
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-01 13:45:15 -0500

    Update App.js

--- author-v2/src/App.js
+++ author-v2/src/App.js
@@ -15,6 +15,7 @@ function App() {
         <Route path='/blogs' element={<BlogSection />} />
         <Route path='/thrillers' element={<Thrillers />} />
         <Route path='/kidfriendly' element= {<KidFriendly />} />
+        <Route path='/*' element = {<LandingPage/>} />
       </Routes>
     </>
   );
