e7ee5a82798c155d4e95b47a92ecea37d54aa427
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-03 12:09:19 -0500

    Update BlogSection.js

--- author-v2/src/Components/BlogSection.js
+++ author-v2/src/Components/BlogSection.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
-import './BlogSection.css';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./BlogSection.css";
 import { Twirl as Hamburger } from "hamburger-react";
 import Header from "./Header";
-import Nav from './Nav';
+import Nav from "./Nav";
 import Copyright from "./Copyright";
+import BlogHome from "../Blogs/BlogHome";
+import Pantser from "../Blogs/Pantser";
 
 function BlogSection() {
   const [isOpen, setOpen] = useState(false);
@@ -12,18 +15,19 @@ function BlogSection() {
     <>
       {/* TODO: Create addition files holding blogs; create links for individual blogs, look into building a comments section */}
       <Header />
-      <div className="nav-menu">
-        <Hamburger toggled={isOpen} toggle={setOpen} />
-        {isOpen ? <Nav /> : null}
+      <div className="blog-section">
+        <div className="nav-menu">
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+          {isOpen ? <Nav /> : null}
+        </div>
+        <Routes>
+          <Route index element={<BlogHome />} /> 
+          <Route path="pantser" element={<Pantser />} />
+        </Routes>
       </div>
-      <h2>Blogs (Welcome to My Mind /The Unusually Named Writer)</h2>
-      <p>
-        nav section leading to blogs, maybe a visual. Include comment section in
-        the blogs?
-      </p>
       <Copyright />
     </>
   );
 }
 
-export default BlogSection
+export default BlogSection;
