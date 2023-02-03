5a3987f47c1cd8996b6c1c6cf62033269c5a5dfc
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-03 12:09:30 -0500

    Create Pantser.js

---
+++ author-v2/src/Blogs/Pantser.js
@@ -0,0 +1,146 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Pantser() {
+  return (
+    <>
+      <br />
+      <NavLink className="blog-nav-link" to="/blogs">
+        ← back
+      </NavLink>
+
+      <div className="blog-header">
+        <h2 className="blog-h2">
+          Why We Need To Stop Labeling People As ‘Pantsers’
+        </h2>
+        <h3 className="blog-h3">
+          An off-the-cuff view of why I won’t steal your pants.
+        </h3>
+      </div>
+
+      <p className="blog-p">
+        HERE YE HERE YE! The (sometimes) honorable Dicky Kitchen Jr will preside
+        over the case against the label of ‘Pantser’ as a term for those who
+        don’t plan every step before writing.
+      </p>
+      <br />
+      <p className="blog-p">
+        The verdict is guilty! The punishment is DEATH!!! (Death of the term,
+        not you. You’re fine... probably).
+      </p>
+      <br />
+      <p className="blog-p">
+        The term is now stricken from the writing community and never to be used
+        again!...
+      </p>
+      <br />
+      <p className="blog-p">
+        Not enough to convince you? Ok, let me try a little harder.
+      </p>
+      <br />
+      <p className="blog-p">
+        How is it we ever came to use this term to describe writers? The other
+        side gets the term ‘planner’; a term seen with some level of dignity and
+        intelligence. Clearly I was absent the day we all had a meeting and
+        decided to call one side ‘planners’ and the other side ‘pantsers’. Had I
+        of been present, I assure you I would have voted for another term and
+        come up with a better alternative.
+      </p>
+      <br />
+      <p className="blog-p">
+        My issue with the term ‘pantser’ is simple. It’s the most inaccurate way
+        to describe what I (and many others) really do.
+      </p>
+      <br />
+      <p className="blog-p">
+        Let me ask you something. Has a non-planner ever surprised you by coming
+        up and yanking your pants down? Because that’s what I pictured when I
+        heard the term ‘pantser’ for the first time and was horrified to learn
+        that I am considered a member of that group.
+      </p>
+      <br />
+      <p className="blog-p">
+        Seriously, it sounds like a potential crime to be a ‘pantser’. It’s
+        lacking the dignity and due respect often given to our colleagues in the
+        other camp.
+      </p>
+      <br />
+      <p className="blog-p">
+        A ‘pantser’ is defined by the ability to sit down and start writing with
+        little to no forethought and still create masterpieces of the
+        imagination! That sounds more like a writing wizard than a pants thief.
+        Surely there is a better term to use to describe us.
+      </p>
+      <br />
+      <ul className="blog-ul">
+        <li>Free Writer</li>
+        <li>Storyteller</li>
+        <li>Story Painter</li>
+        <li>No-Rules Writer</li>
+        <li>Writing Wizard</li>
+        <li>Impulse Writer</li>
+        <li>Creative Genius</li>
+        <li>Conduit to Worlds Unseen</li>
+      </ul>
+      <br />
+      <p className="blog-p">
+        Ok, ok, maybe I’m being a bit generous with a couple of them. But
+        there’s no denying that there has to be a better option than the current
+        model.
+      </p>
+      <br />
+      <p className="blog-p">
+        Personally, I like the term ‘Free Writer’ best. It encompasses what I do
+        much better than the current insult being used. When you hear the term
+        ‘free writer’ you can picture a person sitting down and writing freely,
+        with no structured layout. That is what my colleagues and I do.
+      </p>
+      <br />
+      <p className="blog-p">
+        We shouldn’t be shamed for our unique approach to writing. If anything,
+        you should see our ability as the ultimate superpower for a writer. What
+        is a free writer if not someone with an imagination not confined to
+        rigid structure? Being able to create so freely shows the strength of
+        our creativity. It shows that we are more intuitively capable of
+        developing worlds and creating characters. We have the ability to
+        experience a story’s plot and report it back to you, the reader, without
+        page upon page of notes. We DO NOT steal your pants.
+      </p>
+      <br />
+      <p className="blog-p">
+        If you are a planner and going through your process, you are planning.
+        Correct? So the same way of thinking should apply to us, right?
+      </p>
+      <br />
+      <p className="blog-p">So, are we pantsing?</p>
+      <br />
+      <p className="blog-p">
+        No, because by definition that is the act of pulling someone’s pants
+        down without their consent, and we would never do that. As a rule, we
+        always ask for permission before removing someone’s pants.
+      </p>
+      <br />
+      <p className="blog-p">
+        I think it’s beyond time for us to retire the term ‘pantser’. And by
+        ‘retire’ I mean take it out in a field and bury it, never to be found
+        again. The only other alternative is that we free writers start living
+        up to our less-than-ideal label and go around pantsing all the planners
+        out there.
+      </p>
+      <br />
+      <p className="blog-p">
+        So what’s it going to be? Are we changing the label, or do I have to
+        start stealing pants?
+      </p>
+      <br />
+      <div className="pantser-gif">
+        <img
+          src="https://media1.tenor.com/images/2378092fec0f4056513b0eeb4b2d3335/tenor.gif?itemid=13396071"
+          alt="Spongebob Squarepants pantsing Patrick"
+        />
+      </div>
+    </>
+  );
+}
+
+export default Pantser;
