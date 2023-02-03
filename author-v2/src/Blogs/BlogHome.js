0b069f59992549361c2cdc44558d8657b8222fd3
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-03 14:00:13 -0500

    Update BlogHome.js

--- author-v2/src/Blogs/BlogHome.js
+++ author-v2/src/Blogs/BlogHome.js
@@ -7,8 +7,8 @@ import Portal from '../Images/through-the-wall.webp';
 function BlogHome() {
   return (
     <>
-      <div className='blog-home-intro'>
-        <img src={Portal} alt="author" />
+      <div className="blog-home-intro">
+        <img className='mugshot' src={MugShot} alt="author" />
         <h2 className="blog-h2">
           Welcome To The Mind Of The Unusually Named Writer
         </h2>
@@ -16,23 +16,23 @@ function BlogHome() {
           So you want to dive deeper into the unknown crevices of Dicky Kitchen
           Jr's mind?
         </p>
-        <br/>
         <p className="blog-p">
           Be warned... We don't really know what he is going to do next...
         </p>
-        <br/>
         <p className="blog-p">Go ahead... Pick a blog post and enjoy</p>
       </div>
-      <nav>
-        <NavLink to='pantser'>Pantser</NavLink>
-        <NavLink>Place Holder 2</NavLink>
-        <NavLink>Place Holder 3</NavLink>
-        <NavLink>Place Holder 4</NavLink>
-        <NavLink>Place Holder 5</NavLink>
-        <NavLink>Place Holder 6</NavLink>
-        <NavLink>Place Holder 7</NavLink>
-        <NavLink>Place Holder 8</NavLink>
-        <NavLink>Place Holder 9</NavLink>
+      <nav className="blog-section-nav">
+        <NavLink className="blog-section-nav-link" to="pantser">
+          <h3>Pantser</h3>
+        </NavLink>
+        <NavLink className="blog-section-nav-link"><h3>Place Holder 2</h3></NavLink>
+        <NavLink className="blog-section-nav-link"><h3>Place Holder 3</h3></NavLink>
+        <NavLink className="blog-section-nav-link"><h3>Place Holder 4</h3></NavLink>
+        <NavLink className="blog-section-nav-link"><h3>Place Holder 5</h3></NavLink>
+        <NavLink className="blog-section-nav-link"><h3>Place Holder 6</h3></NavLink>
+        <NavLink className="blog-section-nav-link"><h3>Place Holder 7</h3></NavLink>
+        <NavLink className="blog-section-nav-link"><h3>Place Holder 8</h3></NavLink>
+        <NavLink className="blog-section-nav-link"><h3>Place Holder 9</h3></NavLink>
       </nav>
     </>
   );
