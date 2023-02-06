c1441149eaa0d9481b108ed6ddb4c9a07184cc8c
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-06 14:14:53 -0500

    Create TalkingToMyself.js

---
+++ author-v2/src/Blogs/TalkingToMyself.js
@@ -0,0 +1,490 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+function TalkingToMyself() {
+  return (
+    <>
+      <br />
+      <NavLink className="blog-nav-link" to="/blogs">
+        ← back
+      </NavLink>
+
+      <div className="blog-header">
+        <h2 className="blog-h2">Talking With My Younger Self</h2>
+        <h3 className="blog-h3">
+          A Retrospective Look at My First Year Self-Publishing
+        </h3>
+      </div>
+
+      <p className="blog-p">
+        It took 17 years to write and self-publish my first book. What stated as
+        a short piece of writing in 2003, would become the second chapter in my
+        2020 debut novel - Prey/Pray: Origin of The Average Man.
+      </p>
+      <br />
+      <p className="blog-p">
+        Looking back on that time, I’d love to say it took so long to complete
+        because I regularly worked on it and tweaked the book, searching for
+        perfection in the story over those 17 years. But in reality, I spent the
+        time dragging my feet and ignoring the voice in my head telling me to
+        finish writing the story.
+      </p>
+      <br />
+      <p className="blog-p">
+        In truth, I never wrote the story intending to publish it. As well
+        received as the book has been, it was only originally meant to be
+        written for my entertainment and maybe to share with a friend or two. I
+        didn’t even have the idea to publish it until I completed the story,
+        which made marketing the book a much slower and more painful process,
+        but that’s a discussion for another day.
+      </p>
+      <br />
+      <p className="blog-p">
+        Thinking about it now, I wonder what I would tell my younger self about
+        the book and the process. What would my 40-year-old self tell my
+        22-year-old self one year after having published my first book and
+        having had the time to look back on everything? Let’s find out…
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22 (me from 2003):</span> Hmm… This is
+        different… What’s going on? Where am I? Why is everything so dark?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40 (me now, 2021):</span> Hey.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> **startled, throws a fast punch**
+      </p>
+      <br />
+      <p className="blog-p">THUD</p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> OW!
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> I don’t know who you are, but you’ve
+        got less than a second to explain!
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Geez I was fast back then **rubs jaw**
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Start making sense, or start getting
+        hurt. Your choice.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Calm down. I’m you, stupid.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> No, I’m me. You’re some old guy
+        that’s about to get hit again.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> I’m you from the future. 18 years from
+        now.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> **looks 40 over** Uh, no. Not buying
+        it. Maybe I hit you too hard. Are you so soft that one hit made you
+        stupid, or is that your default setting?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> **rubs face in frustration** I should
+        have really thought this out better. An introduction at a distance would
+        have been safer. It’s not like I was less sarcastic at 22 years old.
+        Ugh. I am you.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Nope. Time to hit you again.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> **throws hands up defensively** Just
+        listen for a second. We’re never going to get through this at this rate.
+        Our name is Dicky Kitchen Jr, our favorite drink is tea, our best friend
+        is a guy that’s fought us more times than any enemy, and we have a scar
+        on our right middle finger from hot poisonous wax gluing our fingers
+        together when we were young. **holds up right middle finger**
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> **cautiously inspects both his and
+        40’s middle finger** Ok… Anyone could have a scar like that though.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> And the other stuff I mentioned?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> I don’t know, maybe you’ve just been
+        watching me.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Look, you have other scars and more
+        personal stuff I could say, but we’re going to share this with other
+        people so it’s probably best to not over share.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Other people? Since when do we share
+        personal stuff with other people?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> It’s a recent development, and
+        honestly still a work in progress. It’s taken you a while. I don’t think
+        I need to explain to you why.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> **relaxes some** Yeah, ok, you’re
+        probably me. But you understand why I didn’t believe you, right? I mean,
+        not only is this nuts, but… **waves a hand at every part of 40**
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> **shrugs** Yeah, there’s work to be
+        done.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> And the hair. Since when do we have
+        hair?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> On and off for the last 5 or so years.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Why?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Got bored.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> **thinks it over** Ok, I can see that
+        happening.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Listen, we need to get on track here.
+        People reading this conversation aren’t here for existential weirdness.
+        We’re here to talk about what we wrote.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> What we wrote? You mean what I just
+        wrote tonight?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Yep. That’s the one. Let’s see, what
+        did we call it back then?... Diary of a Serial Killer?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Yeah. I just wrote that. Not sure I’m
+        sold on the title. Something about it doesn’t work.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> We’ll change it a few times before
+        it’s done.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Done? Wait, does that mean I actually
+        finish writing the story?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Yeah, we finally finish one of the
+        stories we start.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> That’s great! How long did it take to
+        finish?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> 17 years.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Geez! Why so long?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> A few things got in our way. We
+        focused on other things in life, we didn’t believe we could actually
+        finish it, and in a few years you’re going to hear about a show called
+        ‘Dexter’ and think the premise of the show is too similar to what you
+        have in mind for the story. You’ll actually go out of your way to never
+        watch the show, just to make sure no one can accuse you of copying the
+        idea.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Ok, well now that I know all of that,
+        I can change it right? I can just go back and work on it more right
+        away. Or did it suck and I shouldn’t bother?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> No, actually it turned out great. It’s
+        getting fantastic reviews. But, no, you can’t go back and change
+        anything. That’s not how these sorts of things work. Changing the future
+        would prevent you from ever having this conversation, and so you’d never
+        change the future. Besides, you’ll forget all of this when you go back
+        to your time.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Why?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> **shrugs** It’s the only way any of
+        this makes sense.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Fair point.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Anyway, it’s been a year since you’ve
+        published the book. And like I said, the reviews have been
+        overwhelmingly positive. It’s really been a great experience.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> That’s crazy. I can’t believe people
+        actually like my writing. I haven’t even given the main character a name
+        yet. What do I end up naming him?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Um, well, actually… he still doesn’t
+        have a name. None of the important characters in the book do.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Damn. Really?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Yeah, and there’s no cursing in the
+        book either.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> There’s no cursing?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Nope.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> In a book about a serial killer?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Yep.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> So, you’re telling me I end up
+        writing a story…
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> **interrupts** Novel, actually.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Ok, so I write a novel about a serial
+        killer where no one has names, and there’s no cursing, and people are
+        actually enjoying it instead of using it to kindle?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> **chuckles** Kindle, that’s funny.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Why?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> You wouldn’t get it. Consider it a
+        future joke.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Ok.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> But to answer your question, yeah.
+        That’s exactly what you do. There are a couple names in the book, but
+        those are just nods to friends.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> What about the ending? Do I use the
+        ending I have in mind now?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> You expand on it a little, but yeah,
+        you use the ending.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> **smiles largely** And it works?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> It blows peoples minds. 99% of readers
+        never see it coming.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> That’s awesome. So, did some big
+        publisher pick it up or something?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> No, you actually decide to
+        self-publish.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> You can do that?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Yeah, it’s actually a pretty big thing
+        in my time. Millions of people choose to self-publish.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Why do I do it?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> By the time you decide to publish, you
+        really want full control over the story. Instead of risking a
+        traditional publisher telling you to make changes, you decide to do it
+        all yourself.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> Was it the right decision?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Hard to say at this point.
+        Self-publishing is definitely harder than traditional publishing because
+        of how much you have to do on your own. Marketing is probably easier
+        with traditional publishing, as long as you get a good publisher. And
+        the initial cost of self-publishing is more than you’d expect, but it’s
+        necessary to make sure the book appears professional as the
+        traditionally published alternatives. But on the flip side, you get
+        higher royalties self-publishing and own all the rights to your book and
+        characters. At only a year out from the release date, it’s still too
+        early to say if self-publishing was the best decision.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> So we’re not famous?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> **chuckles** Nah, not yet. But who
+        knows? It could happen. Getting a book in the hands of readers takes a
+        lot of time and effort. It’s a bit of a never-ending process. And when
+        it really comes down to it, one book alone seldom gets a writer big
+        recognition. Typically, an author has several books written before they
+        really get noticed.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> So, I have to write more books? You
+        just said it took 17 years just to write the one!!! Great, I’ll be dead
+        before I even write 3 books.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Actually, you’ve already published
+        your first 3 books. The one you just started writing inspires you to
+        write a companion book exploring the back stories of the characters
+        created in the first book. You released the first part of that as your
+        second published book, and then you wrote a kid’s book and published it
+        with an author who is also an illustrator.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> I write a children’s book? That can’t
+        be a good idea. No way did my mind produce a book safe for kids.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Actually, it’s a really cute
+        reimagining of Peter Piper. It’s a great book for kids.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> What possessed me to write a
+        children’s book?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Our son did. You were teaching him the
+        tongue twister, and it turned into something much sillier.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">22:</span> We have a son?!?!?!?!?!? **wobbles
+        and faints**
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">40:</span> Huh… I really should have seen that
+        coming. I never thought I’d be a dad… Oh well, not exactly how I was
+        planning on ending this conversation with myself, but I guess it’s as
+        good as any.
+      </p>
+      <br />
+      <p className="blog-p">POOF. Back to reality.</p>
+      <br />
+      <p className="blog-p">
+        I hope you’ve enjoyed watching me talk to myself. What questions would
+        you ask your past self, and how do you think you’d answer?
+      </p>
+      <br />
+    </>
+  );
+}
+
+export default TalkingToMyself
