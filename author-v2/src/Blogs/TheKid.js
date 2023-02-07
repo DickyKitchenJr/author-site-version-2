b4651a5e3da78aef5bc56faacff49db9aa6c1857
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-07 14:10:52 -0500

    Create TheKid.js

---
+++ author-v2/src/Blogs/TheKid.js
@@ -0,0 +1,263 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function TheKid() {
+  return (
+    <>
+      <br />
+      <NavLink className="blog-nav-link" to="/blogs">
+        ← back
+      </NavLink>
+
+      <div className="blog-header">
+        <h2 className="blog-h2">The Kid</h2>
+        <h3 className="blog-h3">An Unedited Sneak Peek</h3>
+      </div>
+
+      <p className="blog-p">
+        First, there was The Average Man; a serial killer that was anything but
+        average.
+      </p>
+      <br />
+      <p className="blog-p">
+        Then there was The Nurse; a woman with the skills to heal, and the rage
+        to destroy.
+      </p>
+      <br />
+      <p className="blog-p">Next to be introduced is The Kid.</p>
+      <br />
+      <p className="blog-p">
+        Think you know the truth about his origin? Was The Average Man honest in
+        his telling of the tale?
+      </p>
+      <br />
+      <p className="blog-p">
+        There's only one way to find out, and it all starts here...
+      </p>
+      <br />
+      <p className="blog-p">
+        To everyone who has read Prey/Pray: Origin of The Average Man and
+        Prey/Pray: Hunting Party – The Nurse, this sneak peek is for you.
+      </p>
+      <br />
+      <p className="blog-p">
+        For anyone who hasn't read my first two books, click the titles to find
+        them on Amazon. It's strongly advised that you read them before
+        proceeding. Which is to say...
+      </p>
+      <br />
+      <p className="blog-p" style={{ textAlign: "center", fontWeight: "bold" }}>
+        SPOILER ALERT!!!!
+      </p>
+      <br />
+      <p className="blog-p">Still here?</p>
+      <br />
+      <p className="blog-p">
+        Ok then. Enjoy the first writings of 'Prey/Pray: Hunting Party - The
+        Kid'
+      </p>
+      <br />
+      <br />
+      <h3
+        className="blog-p"
+        style={{ textAlign: "center", fontWeight: "bold", textDecoration: 'underline' }}
+      >
+        Prey/Pray: Hunting Party - The Kid
+      </h3>
+      <br />
+      <br />
+      <p className="blog-p">A light rain drizzled down as they drove.</p>
+      <br />
+      <p className="blog-p">
+        “So, how’s your latest project coming along son?” his dad asked, glacing
+        over his shoulder from the driver’s seat.
+      </p>
+      <br />
+      <p className="blog-p">
+        “It’s going ok dad. I’m working on a new game.” He replied.
+      </p>
+      <br />
+      <p className="blog-p">
+        But it wasn’t him. It was his voice, but something was wrong.
+      </p>
+      <br />
+      <p className="blog-p">“Focus on the road honey.” His mother said.</p>
+      <br />
+      <p className="blog-p">
+        “Don’t worry, the rain isn’t that bad. I’ve got this.” His father
+        replied, looking back at the road.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Yeah mom, dads got this” he began to say, but was cut off by his
+        father’s booming voice.
+      </p>
+      <br />
+      <p className="blog-p">“WHAT ARE THEY DOING??? LOOK OUT!!!”</p>
+      <br />
+      <p className="blog-p">
+        A violent force paired with a deafening boom rocked the car, and for a
+        moment time ceased to exist.
+      </p>
+      <br />
+      <p className="blog-p">
+        In that moment, he was able to take in everything around him.
+      </p>
+      <br />
+      <p className="blog-p">
+        His father’s hands clenched around the steering wheel.
+      </p>
+      <br />
+      <p className="blog-p">
+        His mother’s arms thrown up defensively, as if their presence could stop
+        what was to come.
+      </p>
+      <br />
+      <p className="blog-p">
+        And the odd realization that what was once the direction the car was
+        facing, was now located out the passenger window.
+      </p>
+      <br />
+      <p className="blog-p">
+        As the car twisted perpendicular to the road, time unfroze, and the
+        tires lost contact with the ground.
+      </p>
+      <br />
+      <p className="blog-p">
+        Friction is a funny thing. Apply friction to tires straight on, and they
+        roll. However, apply the same friction to the side of the tire, and they
+        can cause a 3000-pound car to flip.
+      </p>
+      <br />
+      <p className="blog-p">
+        The once peaceful inside of the car became a violent arena where
+        everything around them attacked the passengers with each slam against
+        the concrete.
+      </p>
+      <br />
+      <p className="blog-p">
+        Images blurred as his body was tossed in every direction, the seatbelt
+        the only thing keeping him under any semblance of control. The ceiling
+        and sides of the car inched closer to him with every impact, and he
+        heard the screams of his parents.
+      </p>
+      <br />
+      <p className="blog-p">
+        With a final thud, the car finally stopped it’s tumbling, and came to
+        rest upside down.
+      </p>
+      <br />
+      <p className="blog-p">
+        Disoriented and feeling woozy, he began to look around.
+      </p>
+      <br />
+      <p className="blog-p">
+        He felt something warm running down his face, up to his head, and he
+        looked to the driver’s side of the car.
+      </p>
+      <br />
+      <p className="blog-p">He saw his father looking back at him.</p>
+      <br />
+      <p className="blog-p">
+        “Dad, I don’t feel right.” He said, as his world began to close in on
+        him.
+      </p>
+      <br />
+      <p className="blog-p">
+        But his dad just stared back, eyes unblinking and unfocused.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Dad?” he meekly said, as a dark circle overtook the periphery of his
+        vision.
+      </p>
+      <br />
+      <p className="blog-p">
+        It was then that he noticed blood coming from his dad’s ear, dripping
+        onto the ceiling of the car.
+      </p>
+      <br />
+      <p className="blog-p">
+        It was the last thing he saw before the whole world went dark.
+      </p>
+      <br />
+      <p className="blog-p" style={{ textAlign: "center" }}>
+        ****
+      </p>
+      <br />
+      <p className="blog-p">
+        He gasps as he shoots forward in the seat, the seatbelt yanking him back
+        as his palms slam against the glovebox in front of him.
+      </p>
+      <br />
+      <p className="blog-p">“Geez!” the driver shouts.</p>
+      <br />
+      <p className="blog-p">
+        “Kid, are you ok?” she says, recovering from the shock of the boy’s
+        sudden motion.
+      </p>
+      <br />
+      <p className="blog-p">
+        He looks around, eyes wide in horror, and realizes he was dreaming.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Kid. It’s ok. You’re ok.” She says, reaching out and touching his arm.
+      </p>
+      <br />
+      <p className="blog-p">His pulse calms and he glances down at her hand.</p>
+      <br />
+      <p className="blog-p">
+        “Yeah. I’m ok. Sorry. I didn’t mean to startle you.” He says.
+      </p>
+      <br />
+      <p className="blog-p">“Was it the dream again?” she asks.</p>
+      <br />
+      <p className="blog-p">
+        “Yeah.” He replies, looking down, wiping the moisture from his eyes.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Hey, it’s ok. We’re almost there, but if you need me to, I’ll pull over
+        for a minute. We don’t have to keep driving. We’re making good time.”
+        She says, looking concerned.
+      </p>
+      <br />
+      <p className="blog-p">
+        “No. Let’s just keep going. I’m ok. Sorry. I didn’t mean to doze off.
+        I’m good now. Honest. Let’s just keep going.” He nods, taking a deep
+        breath and exhaling.
+      </p>
+      <br />
+      <p className="blog-p">“Are you sure Kid?”</p>
+      <br />
+      <p className="blog-p">
+        “Yeah, I’m sure. Let’s get this done, so you can stop calling me Kid.”
+      </p>
+      <br />
+      <p className="blog-p">
+        She smiles. “You know the rules, no names when we’re working.”
+      </p>
+      <br />
+      <p className="blog-p">
+        “I guess that’s one way to look at what we’re doing.” He says, cocking
+        an eyebrow.
+      </p>
+      <br />
+      <p className="blog-p">“You like his way of viewing it better?”</p>
+      <br />
+      <p className="blog-p">
+        “Maybe. It definitely sounds cooler. Don’t you think so Nurse?” he says
+        with an edge of sarcasm to the name Nurse.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Ok Kid,” she mockingly emphasizes as he did, “have it your way. No
+        names while we’re hunting.”
+      </p>
+      <br />
+    </>
+  );
+}
+
+export default TheKid;
