c5808d4440e2175babd661a202ec373469b75993
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-07 11:33:24 -0500

    Create LynnePearson.js

---
+++ author-v2/src/Blogs/LynnePearson.js
@@ -0,0 +1,422 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Lynne from "../Images/Lynne.webp";
+
+function LynnePearson() {
+  return (
+    <>
+      <br />
+      <NavLink className="blog-nav-link" to="/blogs">
+        ← back
+      </NavLink>
+
+      <div className="blog-header">
+        <h2 className="blog-h2">Interview With Editor Lynne Pearson</h2>
+      </div>
+
+      <figure className="blog-figure">
+        <img className="blog-img" src={Lynne} alt="Lynne Pearson" />
+      </figure>
+
+      <p className="blog-p">
+        The most powerful tool a writer can have is a great editor. A great
+        editor can uncover the diamond in the rough. And while it may be painful
+        to have someone chip away at your manuscript to reveal the gem beneath,
+        there is no better person on earth to help transition your story from a
+        wonderful idea to a legible masterpiece.
+      </p>
+      <br />
+      <p className="blog-p">
+        With that in mind, it should come as no surprise that my editor’s name
+        popped into my head the moment I decided I wanted to add interviews to
+        my web content.
+      </p>
+      <br />
+      <p className="blog-p">
+        One of the first times I communicated with Lynne was when she responded
+        to, and promptly recommended edits for, one of my posts on Instagram.
+        Some people would have gotten upset by having a stranger recommend
+        corrections and improvements to a post, but I saw it for the beautiful
+        gift it was.
+      </p>
+      <br />
+      <p className="blog-p">
+        There are two reasons I was happy for the recommendations instead of
+        feeling indignant. One, it’s my firm belief that any and every writer
+        should welcome critique with a smile and an open mind. And two, her
+        recommendations were damn good! So instead of being put off, I thanked
+        her and implemented the changes she recommended.
+      </p>
+      <br />
+      <p className="blog-p">
+        As I continued to post snippets from my debut novel, Lynne continued to
+        pop up with helpful and insightful suggestions, and quickly became the
+        person I wanted to hire for all my editing needs. To date, she has been
+        the editor for Pray: Hunting Party – The Nurse, The True Tale of Peter
+        Piper, and the second edition release of my debut novel - Prey/Pray:
+        Origin of The Average Man.
+      </p>
+      <br />
+      <p className="blog-p">
+        Lynne is one of those people whose passion also lines up with her
+        profession, so much so that when I asked her if she would take part in
+        this interview she replied with: “I am happy to do that for you. I may
+        edit your questions…”
+      </p>
+      <br />
+      <p className="blog-p">
+        So, without further ado, I present to you Lynne Person and my edited
+        questions:
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> Lynne, thanks again for agreeing to
+        do this interview. I think editing is one of those scary things both
+        self-published and traditional authors deal with, and I’m hoping this
+        will help other writers realize editors aren’t inherently evil and what
+        exactly it is that a good editor can offer. Now I know there are
+        different types of editing, but can you give us a rundown of the editing
+        you provide and what the different types mean?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> Sure. A copy edit is the simplest
+        level of editing I offer. That’s correcting errors in capitalization,
+        spelling, grammar, and punctuation. There are many writing programs that
+        can do that. What a human editor will do is check for consistency – the
+        name of a character is always spelled correctly. For example, my name is
+        Lynne with an E. Yet it can be spelled Lyn, Lynn, Linn. Most software
+        programs are not going to pick that up. A good copy editor will. I will
+        also query the writer about factual inconsistencies and word choice. A
+        writer may use a word that I’m not familiar with or use it in a way that
+        is uncommon. That doesn’t make the word wrong, I just point it out and
+        let them decide.
+      </p>
+      <br />
+      <p className="blog-p">
+        The next level of editing I provide is a line edit. It includes
+        everything in a copy edit but then I dive a little deeper. I look at
+        timeline, character descriptions, and plot points. I will suggest
+        rewriting sentences and paragraphs for clarity, improve the pace, and
+        deepen the emotion. I bring to the writer’s attention anything I think
+        will bump the reader out of the story.
+      </p>
+      <br />
+      <p className="blog-p">
+        A manuscript critique is an evaluation of the story itself. Does the
+        character have clearly defined goals? Are there gaping plot holes? What
+        is the inciting incident and does the story actually start there? I may
+        suggest adding conflict, shifting scenes, changing point of view. It is
+        a thorough evaluation. I don’t make copy edit corrections on this level
+        because I want the writer to focus on the story.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> I mentioned in the introduction
+        that your passion lines up with your profession, and from the
+        conversations we’ve had, I think it’s fair to say that editing really is
+        a passion for you. When did you first realize that you were passionate
+        about editing?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> I was invited to a neighbourhood
+        book group where a local author would be speaking about her novel. I
+        noticed inconsistencies but did not mention them to the author. At this
+        point there wasn’t a darn thing she could do about them – the book was
+        in print – but other people did. I joined that author’s newsletter and a
+        few months later she shared the first few chapters of her current work
+        in progress. I found inconsistencies in the story – it was set in the
+        past – and she referenced events that hadn’t happened yet. I had no idea
+        how to contact her. That led me to thinking I should offer my services
+        as a reader to her publisher and it dawned on me that editing was a
+        career that interested me. Writers work so darn hard to get their
+        stories out there and who wants their work derailed by typos and
+        mistakes? Funny story, that author became a client.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> And how long have you been editing?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> I finished my editing program from
+        University of Washington in June of 2016 and hung up my shingle that
+        fall.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> In the introduction, I also
+        mentioned that some of our first conversations revolved around edits you
+        recommended for some parts of my book that I was posting online. Is that
+        something you’ve done with a lot of others?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> Nope. Only one other person.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> What is it that makes you reach out
+        and offer to help in a situation like that?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> Respect for writers, and a sincere
+        desire to see them present the best book they possibly can. Also the way
+        you presented yourself. Your posts are engaging and I saw something I
+        could help with so I did.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> For me, one thing that really
+        impressed me about you is you never did any type of hard pitch for your
+        services. It never felt like you were offering suggestions just to get
+        me to pay you for your skills, and if I’m remembering correctly, I think
+        I approached you about taking me on as a client and not the other way
+        around. Having grown up in sales, it was really refreshing to see that
+        you let your skills speak for themselves instead of pushing a sales
+        pitch. Have you come across a lot of clients by just being yourself and
+        letting your inner editor come through naturally?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> I suppose. I’ve never thought of
+        it that way. If someone is having an issue with their book and asks me a
+        question, I’m happy to help.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> When it comes to your clients, I
+        know you work with more than just thriller writers like myself, what
+        different genres have you worked with?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> Memoirs, mysteries, women’s
+        fiction, dystopia, fantasy.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> Do you have a favorite genre to
+        work with?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> The majority of my work comes from
+        romance writers. I love a happy ever after story.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> Are there any genres that you won’t
+        work with, and if so, why?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> I haven’t encountered one yet. Not
+        sure if I have the skill set for sci-fi though. I’m not a fan of books
+        with sexual violence so I might turn that down.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> One of the many things I think you
+        handle amazingly well, is protecting the voice of the writer. A fear I
+        had going into hiring an editor, and a fear I’m sure other writers have
+        experienced, is worrying that an editor would change their story to the
+        point where it didn’t seem like their writing anymore. Is that a skill
+        set you’ve always had, or is something you’ve had to develop?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> I think it has to be developed. An
+        editor has to remember that it is not their book. Starting out, I had
+        conversations with writers where I was overstepping and I had to pull
+        back. Now, if I see something that I think needs a drastic change, I
+        will talk with the writer first. I tell writers that my edits are
+        suggestions and it is up to them whether or not they accept them.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> Is it difficult to maintain a
+        writer’s voice that isn’t your own?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> Yes. But it’s a challenge I fully
+        embrace.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> I’ve been through the process with
+        you a couple times now, so I’m developing a better understanding of it,
+        but for those who haven’t worked with a professional editor, walk me
+        through the steps you take for editing and how you work with the writer
+        to improve their story.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> I start off with a conversation.
+        Preferably a video chat. Stuck in my little office, I like to see faces.
+        I ask authors about their writing background, their publishing goals,
+        their concerns. Then we determine the level of editing they require. I
+        download the manuscript onto my kindle so I’m not tempted to start
+        editing write away. The first editing pass starts with running the
+        manuscript through PefectIt. That helps with inconsistencies. Then I go
+        through the book sentence by sentence, paragraph by paragraph. I edit in
+        Word. I use the comment bubbles to make suggestions, point out issues,
+        and query the author. When I finish the first round, I create a Style
+        Sheet. It includes suggestions for improvement, the reasoning for my
+        edits, and is a printout of all the comments I’ve made in the
+        manuscript. I schedule another conversation with the author to go over
+        the Style Sheet. Then I send that off to the author with the marked-up
+        manuscript for them to make revisions. When they complete their
+        revisions, they send it back to me. I read it one more time and clean it
+        up. Then I return it to the author.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> For those looking for an editor,
+        what are some key things to look for before hiring someone?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span>
+        <ul className="blog-ul">
+          <li>Find someone who is familiar with your genre.</li>
+          <li>Preferably find someone who has taken courses in editing.</li>
+          <li>Determine what level of edit you need.</li>
+          <li>Familiarize yourself with the costs of an edit.</li>
+          <li>
+            The Editorial Freelancers Association is an excellent resource.
+          </li>
+        </ul>
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> And what are some red flags to look
+        out for?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span>
+        <ul className="blog-ul">
+          <li>Non-professional conduct.</li>
+          <li>
+            If they trash talk a client to you, chances are they will do the
+            same to you.
+          </li>
+          <li>Don’t hire the cheapest editor you can find.</li>
+          <li>Don’t let them rewrite your book.</li>
+        </ul>
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> What are some things writers can do
+        to make a partnership with an editor more successful?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span>
+        <ul className="blog-ul">
+          <li>Clarify exactly what you are looking for.</li>
+          <li>
+            Ask questions if you don’t understand why they’ve made a suggestion.
+          </li>
+          <li>
+            If you’ve sent your manuscript to your editor, don’t tinker with it
+            and resend it. Chances are you’ve just created more work for them.
+          </li>
+        </ul>
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> In addition to being an editor,
+        you’re also a writer, and we’ve spoken before about how you don’t edit
+        for yourself when it comes to your story. Explain to everyone why, even
+        at a professional editor’s level, it’s important to get outside help
+        when it comes to editing something you’ve written.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> I can’t see my mistakes. I’m too
+        close to the story and can’t see if I’ve managed to translate what’s in
+        my head to what’s on the page.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="self">Dicky:</span> Speaking of your story, do you want
+        to tell us about it and what stages of the writing/publishing process
+        you’re currently in?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> I am currently working on a story
+        about two sisters who are trying to launch an event planning company and
+        the setbacks they encounter. I’m about half way through the first draft.
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="">Dicky:</span> Anything else you’d like to add in that
+        I haven’t brought up?
+      </p>
+      <br />
+      <p className="blog-p">
+        <span className="guest">Lynne:</span> I believe that writers need to be
+        readers. It allows you to see what’s popular, provides examples of how
+        to drive a plot and build memorable characters. Read from the best and
+        from the worst. There are lessons to be learned from both.
+      </p>
+      <br />
+      <p className="blog-p">
+        Thank you again to the wonderful Lynne Pearson for agreeing to this
+        interview. If you are interested in hiring Lynne or keeping up with her
+        on social media, you can find her here:
+      </p>
+      <br />
+      <p className="blog-p">
+        Editing Website:{" "}
+        <a href="https://allthatediting.com/" target="_blank" rel="noreferrer">
+          https://allthatediting.com/
+        </a>
+      </p>
+      <p className="blog-p">
+        Author Website:{" "}
+        <a
+          href="https://lynnehancockpearson.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://lynnehancockpearson.com/
+        </a>
+      </p>
+      <p className="blog-p">
+        Facebook:{" "}
+        <a
+          href="https://www.facebook.com/allthatediting"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://www.facebook.com/allthatediting
+        </a>
+      </p>
+      <p className="blog-p">
+        Instagram:{" "}
+        <a
+          href="https://www.instagram.com/allthatediting/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://www.instagram.com/allthatediting/
+        </a>
+      </p>
+      <br />
+      <p className="blog-p">
+        And if you’re wondering how many corrections Lynne made to my post… The
+        number is eleven.
+      </p>
+      <br />
+    </>
+  );
+}
+
+export default LynnePearson;
