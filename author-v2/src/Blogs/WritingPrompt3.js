abfe5183f777de429c05a900b89bdd6949e87a7a
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-08 11:54:37 -0500

    Create WritingPrompt3.js

---
+++ author-v2/src/Blogs/WritingPrompt3.js
@@ -0,0 +1,244 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import WriteSomething from "../Images/write-something.webp";
+
+function WritingPrompt3() {
+  return (
+    <>
+      <br />
+      <NavLink className="blog-nav-link" to="/blogs">
+        ← back
+      </NavLink>
+
+      <div className="blog-header">
+        <h2 className="blog-h2">BookInd Writing Prompt</h2>
+        <h3 className="blog-h3">Prompt from 10/12/2022</h3>
+      </div>
+
+      <figure className="blog-figure">
+        <img
+          className="blog-img"
+          src={WriteSomething}
+          alt="typewriter with 'write something' on the paper in it"
+        />
+      </figure>
+
+      <p className="blog-p">
+        Every week a new writing prompt is added to the BookInd community. The
+        rules are simple - use the prompt or it's essence to create a short
+        piece of writing. The following is one such prompt and my subsequent use
+        of the prompt:
+      </p>
+      <p className="blog-p writing-prompt">
+        Writing Prompt - Two characters square off in a fight or challenge that
+        will define how their future plays out. However, one of them is suddenly
+        slowed down or stopped entirely by the most mundane or ridiculous reason
+        they can think of...
+      </p>
+      <br />
+      <p className="blog-p">
+        The fight had gone on for what felt like hours. The home in shambles, a
+        testament to the war the two men had fought with each other.
+      </p>
+      <br />
+      <p className="blog-p">
+        Twelve rounds of 9mm and ten rounds of 38mm had pock marked the walls,
+        miraculously missing both men. Both guns ran dry before being used as
+        melee weapons.
+      </p>
+      <br />
+      <p className="blog-p">
+        A cast iron frying pan remained suspended from the dry wall, barely
+        missing the head of one of the men as it whizzed through the air.
+      </p>
+      <br />
+      <p className="blog-p">
+        Doors hung from their hinges, holes and cracks from both men being
+        thrown into them at various moments of the altercation.
+      </p>
+      <br />
+      <p className="blog-p">
+        Furniture had been flipped, tables smashed, and lamps shattered.
+      </p>
+      <br />
+      <p className="blog-p">
+        Ground zero for a brawl that would leave most, if not both, combatants
+        dead.
+      </p>
+      <br />
+      <p className="blog-p">
+        And yet they stood bloody and beaten, the only energy remaining provided
+        by their primal urge to survive, facing each other.
+      </p>
+      <br />
+      <p className="blog-p">
+        No words were exchanged. No words were needed. The understanding was
+        clear. They wanted to kill each other.
+      </p>
+      <br />
+      <p className="blog-p">
+        Peter crossed the wrong clients, and Eugene was the person sent in to
+        erase those who made that mistake.
+      </p>
+      <br />
+      <p className="blog-p">
+        The brief pause in violence was broken by a haymaker flying towards
+        Eugene’s head, thrown out of exhaustion and desperation.
+      </p>
+      <br />
+      <p className="blog-p">
+        Eugene ducked the punch and countered with a right cross, connecting
+        squarely in Peter’s midsection, and forcing what little air remained in
+        his lungs to violently expel as he lurched backwards and collapsed
+        against a bookshelf.
+      </p>
+      <br />
+      <p className="blog-p">
+        “It’s over,” Eugene stated, “This could have been done quick with a
+        bullet, but you had to go and fight back. I never would have guessed a
+        dweeb like you could have caused this much trouble.”
+      </p>
+      <br />
+      <p className="blog-p">
+        Peter glared up, the only reply he could make while his body re-learned
+        to process air.
+      </p>
+      <br />
+      <p className="blog-p">
+        Eugene braced his hands on the bookshelf, too tired to trust his own
+        balance, and raised his foot to stomp down on the defenseless target.
+      </p>
+      <br />
+      <p className="blog-p">
+        Defiantly Peter stared up, but he knew he didn’t have the energy to
+        evade the terrible fate to come.
+      </p>
+      <br />
+      <p className="blog-p">
+        Still, he had given it his best. He refused to show any fear even here
+        at the end.
+      </p>
+      <br />
+      <p className="blog-p">
+        Expecting to feel the first of several crushing stomps that would end
+        his life, Peter instead found himself staring at an astonished look on
+        Eugene’s face.
+      </p>
+      <br />
+      <p className="blog-p">
+        “What are you waiting for?” Peter wheezed; his breathing still labored
+        by the previous blow.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Is that…” Eugene cocked his head to the side and peered at something
+        behind Peter.
+      </p>
+      <br />
+      <p className="blog-p">Puzzled, Peter just continued to stare up.</p>
+      <br />
+      <p className="blog-p">
+        “Is that a copy of The Amazing Spider-Man #299, released in April 1988?
+        The first appearance of Eddie Brock as Venom?” Eugene asked.
+      </p>
+      <br />
+      <p className="blog-p">
+        Perplexed, Peter slowly glanced over his shoulder seeing the comic in
+        its protective wooden from sitting on the bookshelf his head rested on.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Yeah, signed by Todd McFarlane.” Peter said, returning his eyes to
+        Eugene.
+      </p>
+      <br />
+      <p className="blog-p">
+        “No freaking way.” Eugene gasped, putting his foot down taking a step
+        back.
+      </p>
+      <br />
+      <p className="blog-p">
+        The two stared at each other for a moment before Peter broke the
+        silence.
+      </p>
+      <br />
+      <p className="blog-p">
+        “It’s one of two copies I own. I keep the other locked up.”
+      </p>
+      <br />
+      <p className="blog-p">
+        “Wow.” Eugene stated, the killer’s face adopting a more boyish
+        expression.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Do you…” Peter paused a second, “Do you want that copy?”
+      </p>
+      <br />
+      <p className="blog-p">
+        “Seriously?” Eugene looked like he just won the science fair.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Well, yeah. I mean, if it ends…” Peter gestured around with his hand,
+        highlighting the devastation surrounding them.
+      </p>
+      <br />
+      <p className="blog-p">
+        Eugene looked around at the mayhem that surrounded them.
+      </p>
+      <br />
+      <p className="blog-p">“You’ve got a deal.”</p>
+      <br />
+      <p className="blog-p">
+        Peter cautiously raised from the ground and handed the framed comic to
+        Eugene.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Man, this is just… wow.” Eugene said grinning down at the comic.
+      </p>
+      <br />
+      <p className="blog-p">
+        As the totality of the situation sank in, Eugene glanced at the front
+        door.
+      </p>
+      <br />
+      <p className="blog-p">
+        “I’m going to leave now. Um…” he shrugged and backed towards the door.
+      </p>
+      <br />
+      <p className="blog-p">
+        Peter stared as the man stepped over rubble towards the door.
+      </p>
+      <br />
+      <p className="blog-p">
+        As he began to step through the threshold and exit the home, Eugene
+        looked back over his shoulder at Peter.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Listen, Pete…” he began, “you should consider leaving. Me failing here
+        doesn’t mean they’ll stop.”
+      </p>
+      <br />
+      <p className="blog-p">“I know.” Peter replied.</p>
+      <br />
+      <p className="blog-p">
+        “The Six are coming for you Pete,” Eugene gave one more look at the
+        destruction in the home, “Sorry about the mess.”
+      </p>
+      <br />
+      <p className="blog-p">
+        Eugene shut the door behind him, and Peter collapsed to the floor.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Saved by Spider-Man,” Peter sighed, “Not how I saw my day going.”
+      </p>
+      <br />
+    </>
+  );
+}
+
+export default WritingPrompt3
