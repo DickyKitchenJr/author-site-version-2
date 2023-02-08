256facfbbffd19891447c441cbda95471e824cda
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-08 14:12:41 -0500

    Create WritingPrompt4.js

---
+++ author-v2/src/Blogs/WritingPrompt4.js
@@ -0,0 +1,324 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import WriteSomething from "../Images/write-something.webp";
+import HauntedHouse from "../Images/spooky-field-house.webp";
+
+function WritingPrompt4() {
+  return (
+    <>
+      <br />
+      <NavLink className="blog-nav-link" to="/blogs">
+        ← back
+      </NavLink>
+
+      <div className="blog-header">
+        <h2 className="blog-h2">BookInd Writing Prompt</h2>
+        <h3 className="blog-h3">Prompt from 9/28/2022</h3>
+      </div>
+
+      <figure className="blog-figure">
+        <img
+          className="blog-img"
+          src={WriteSomething}
+          alt="typewriter with 'write something' on the paper in it"
+        />
+      </figure>
+
+      <p className="blog-p">
+        Every week a new writing prompt is added to the BookInd community. The
+        rules are simple - use the prompt or it's essence to create a short
+        piece of writing. The following is one such prompt and my subsequent use
+        of the prompt:
+      </p>
+      <p className="blog-p writing-prompt">Writing Prompt -</p>
+
+      <figure className="blog-figure">
+        <img
+          className="blog-img"
+          src={HauntedHouse}
+          alt="a dilapidated house in a field with eerie aura"
+        />
+        <figcaption className="blog-fig-cap">Visual Writing Prompt</figcaption>
+      </figure>
+
+      <p
+        className="blog-p"
+        style={{
+          textAlign: "center",
+          fontWeight: "bold",
+          textDecoration: "underline",
+          color: "red",
+        }}
+      >
+        TRIGGER WARNING
+      </p>
+      <p
+        className="blog-p"
+        style={{
+          textAlign: "center",
+          fontStyle: "italic",
+          color: "red",
+        }}
+      >
+        This short contains graphic descriptions of violence, gore, and horror
+        which some readers may find it unsettling. Continue reading as you wish,
+        or hit the back button at the top of the page to select a different blog
+        post.
+      </p>
+      <br />
+      <br />
+
+      <p className="blog-p">
+        On a break from college Alice and Danny decided to take a road trip
+        across the state, but four hours into the trip the car had broken down.
+      </p>
+      <br />
+      <p className="blog-p">
+        The bleak road the car died on offered little in help, with no cell
+        phone reception and an empty field on both sides. Empty save for a lone
+        house seen in the distance.
+      </p>
+      <br />
+      <p className="blog-p">
+        A single wooden post impaled into the ground with the words “Keep Out”
+        in chipped red paint stood on the property’s perimeter.
+      </p>
+      <br />
+      <p className="blog-p">
+        There was a brief conversation about respecting the sign’s request, but
+        with no cars seen traveling the road and darkness quickly approaching,
+        the couple agreed to walk through the field in hopes of finding help at
+        the house in the distance.
+      </p>
+      <br />
+      <p className="blog-p">
+        As Alice and Danny pushed through the thick reeds and stalks of the
+        field, Alice had frozen at a sound.
+      </p>
+      <br />
+      <p className="blog-p">“What is it?” Danny asked.</p>
+      <br />
+      <p className="blog-p">
+        “I don’t know,” Alice replied, “I thought I just heard whispering.”
+      </p>
+      <br />
+      <p className="blog-p">
+        Danny glanced around, but after seeing nothing but vegetation,
+        encouraged Alice to keep moving.
+      </p>
+      <br />
+      <p className="blog-p">
+        After several minutes, Alice heard the whisper again, and grabbed Danny
+        by the arm.
+      </p>
+      <br />
+      <p className="blog-p">“You don’t hear that?”</p>
+      <br />
+      <p className="blog-p">“Hear what?”</p>
+      <br />
+      <p className="blog-p">Danny’s face furrowed in confusion.</p>
+      <br />
+      <p className="blog-p">
+        “The whispering! I think we should go back to the car. Somethings not
+        right. There’s someone out here. Let’s just go back to the car and wait
+        for someone to drive by.” Alice pleaded.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Alice you’re just being paranoid. It’s just the wind blowing through
+        the plants. There’s nothing out he…”
+      </p>
+      <br />
+      <p className="blog-p">
+        A blur erupted out of the vegetation to Danny’s right, and his body flew
+        into the air, a pink mist floating in the space he occupied a second
+        before.
+      </p>
+      <br />
+      <p className="blog-p">
+        Alice stood in shock, the taste of copper on her tongue, and looked down
+        where Danny landed.
+      </p>
+      <br />
+      <p className="blog-p">
+        A red puddle formed under Danny, a look of disbelief on his face as he
+        gasped for air.
+      </p>
+      <br />
+      <p className="blog-p">
+        He put a hand on the ground to push himself up, but only the top half
+        moved. Sinews separated releasing his intestines onto the ground,
+        revealing a jagged tear across his midsection.
+      </p>
+      <br />
+      <p className="blog-p">Danny screamed as pain washed over him.</p>
+      <br />
+      <p className="blog-p">
+        Alice’s panic-stricken eyes darted around, her mind trying to process
+        what occurred just seconds before.
+      </p>
+      <br />
+      <p className="blog-p">
+        Emerging from the reeds behind Danny, a beast’s snout slowly came into
+        view.
+      </p>
+      <br />
+      <p className="blog-p">
+        Black fur covered the creature and its bright red eyes pierced the night
+        as it crept towards Danny.
+      </p>
+      <br />
+      <p className="blog-p">
+        Placing a clawed hand on Danny’s legs, the creature drove its maw into
+        his back. A horrid crack echoed out as the beast’s teeth snapped through
+        Danny’s spine, its victim bellowing in pain.
+      </p>
+      <br />
+      <p className="blog-p">
+        With a shake of its head, the beast separated the remaining flesh
+        connecting the two halves of Danny, his organs the only remaining
+        connection between the gruesome blood-drenched sides.
+      </p>
+      <br />
+      <p className="blog-p">
+        Alice shrieked in horror at the scene before her.
+      </p>
+      <br />
+      <p className="blog-p">
+        The beast released Danny and glanced up at Alice, a smirk painted across
+        its blood-soaked muzzle.
+      </p>
+      <br />
+      <p className="blog-p">
+        The creature turned its head back to its victim and gingerly grabbed
+        Danny’s left leg in his mouth. Then looking back at Alice, bit down with
+        a cruel slowness so that she could see its fangs dig into Danny’s flesh
+        before sprinting off into the vegetation that surrounded them.
+      </p>
+      <br />
+      <p className="blog-p">
+        Time froze as Alice stared down at Danny’s top half, intestines
+        stretching out in the direction the beast had just disappeared, before
+        the tension of his organs finally reached the apex of their elasticity
+        and the remainder of his body was dragged off to follow.
+      </p>
+      <br />
+      <p className="blog-p">
+        Panicked energy found Alice’s feet and she sprinted in the opposite
+        direction, survival her only thought.
+      </p>
+      <br />
+      <p className="blog-p">
+        She ran until her lungs burned. Vegetation whipped her body as she burst
+        through the field leaving welts and scratches on all exposed skin.
+      </p>
+      <br />
+      <p className="blog-p">
+        Stray roots challenged her steps until momentum stole her balance.
+      </p>
+      <br />
+      <p className="blog-p">
+        Alice hit the ground hard, heart beats pounding in her ears.
+      </p>
+      <br />
+      <p className="blog-p">
+        Frantic she looked around and found the house just a few yards away.
+      </p>
+      <br />
+      <p className="blog-p">
+        It appeared much more dilapidated from this distance, but its offer of
+        protection from the beast seemed a fortress compared to the vegetation
+        around her.
+      </p>
+      <br />
+      <p className="blog-p">
+        Alice clawed herself up to a crouched position and scanned her
+        surroundings for the beast.
+      </p>
+      <br />
+      <p className="blog-p">
+        A familiar whisper danced across the air and Alice’s feet began to move
+        without her willing them to do so.
+      </p>
+      <br />
+      <p className="blog-p">
+        Bounding up the steps two at a time, Alice grabbed the front door and
+        burst into the home, unsure if the door had been unlocked or if she had
+        broken in.
+      </p>
+      <br />
+      <p className="blog-p">
+        Secure within the home’s walls she hurried to push the door shut and
+        leaned her back against it, collapsing as she did.
+      </p>
+      <br />
+      <p className="blog-p">
+        Alice sat breathless against the old wooden door and listened for any
+        sign of the beast.
+      </p>
+      <br />
+      <p className="blog-p">
+        As her pulse calmed, she looked around the old home.
+      </p>
+      <br />
+      <p className="blog-p">
+        Its ramshackle interior covered in dust and cobwebs, speaking to a void
+        of any life.
+      </p>
+      <br />
+      <p className="blog-p">
+        Silent moments passed as Alice’s pulse began to lower.
+      </p>
+      <br />
+      <p className="blog-p">
+        Her calm was broken by a childish giggle from somewhere upstairs in the
+        home.
+      </p>
+      <br />
+      <p className="blog-p">
+        “H-h-hello?” Alice said as tears filled her eyes.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Sammy must have really liked you. He doesn’t usually let people get to
+        the house.” a little girl’s voice echoed.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Sammy?” Alice’s voice cracked as the words left her mouth.
+      </p>
+      <br />
+      <p className="blog-p">“My pet silly.” the voice giggled in reply.</p>
+      <br />
+      <p className="blog-p">
+        Alice pushed herself to her feet, eyes shooting around the home looking
+        for anything she could use to protect herself.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Oh, it’s too late for that.” the voice stated taking a darker edge.
+      </p>
+      <br />
+      <p className="blog-p">A click sounded behind Alice as the door locked.</p>
+      <br />
+      <p className="blog-p">
+        Alice yanked at the door desperate to escape the new threat.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Don’t try to run away. You’re my new toy.” the once child-like voice
+        growled.
+      </p>
+      <br />
+      <p className="blog-p">
+        As the sun set, a shutter passed through the old home and Alice’s
+        screams could be heard for miles.
+      </p>
+      <br />
+      <p className="blog-p">If only there were someone to hear them…</p>
+      <br />
+    </>
+  );
+}
+
+export default WritingPrompt4;
