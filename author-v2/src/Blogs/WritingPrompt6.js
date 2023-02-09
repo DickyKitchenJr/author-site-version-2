16fe198ebce9c135c7b65fb8e5844e9d86b90919
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-09 12:30:37 -0500

    Create WritingPrompt6.js

---
+++ author-v2/src/Blogs/WritingPrompt6.js
@@ -0,0 +1,418 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import WriteSomething from "../Images/write-something.webp";
+
+function WritingPrompt6() {
+  return (
+    <>
+      <br />
+      <NavLink className="blog-nav-link" to="/blogs">
+        ← back
+      </NavLink>
+
+      <div className="blog-header">
+        <h2 className="blog-h2">BookInd Writing Prompt</h2>
+        <h3 className="blog-h3">Prompt from 1/04/2023</h3>
+      </div>
+
+      <figure className="blog-figure">
+        <img
+          className="blog-img"
+          src={WriteSomething}
+          alt="typewriter with 'write something' on the paper in it"
+        />
+      </figure>
+
+      <p className="blog-p">
+        Every week a new writing prompt is added to the BookInd community. The
+        rules are simple - use the prompt or it's essence to create a short
+        piece of writing. The following is one such prompt and my subsequent use
+        of the prompt:
+      </p>
+      <p className="blog-p writing-prompt">
+        Writing Prompt - Base a short around a resolution a character made
+      </p>
+      <br />
+
+      <p className="blog-p">
+        Muffled conversations leaked from behind the wooden door, a sign reading
+        “LSA. New members welcome” taped to the front.
+      </p>
+      <br />
+      <p className="blog-p">
+        Stan stood, arms at his side, fist clenched, nervousness flowing through
+        his veins as he debated entering the room.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Palms are sweaty, knees weak, arms are heavy.” He mumbled.
+      </p>
+      <br />
+      <p className="blog-p">
+        Stan shook his head and clenched his fist tighter.
+      </p>
+      <br />
+      <p className="blog-p">
+        “No. That’s why you’re here. You promised everyone you’d try to change.
+        Your New Year’s resolution was to change. You have to do this. You have
+        to try.” He said, steeling his nerves.
+      </p>
+      <br />
+      <p className="blog-p">
+        Reaching his hand out, he gripped the door and pulled it open.
+      </p>
+      <br />
+      <p className="blog-p">
+        The noisy room silenced as he entered. Several people sat in a large
+        circle of chairs facing each other, all looking at Stan as he stood
+        frozen in the doorway.
+      </p>
+      <br />
+      <p className="blog-p">“Haven’t you people ever heard of closing the…”</p>
+      <br />
+      <p className="blog-p">
+        “Ryan! We’ve talked about this. That is not how you great new members.”
+        A man said as he stood from his chair, cutting off the words of the
+        first.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Sorry doc. I’m still working on it.” Ryan replied.
+      </p>
+      <br />
+      <p className="blog-p">
+        “I’m not a doctor. We’ve also been over that. I’m just a counselor
+        trying to help guide you all to a healthier life. I do not have my
+        doctorate.”
+      </p>
+      <br />
+      <p className="blog-p">
+        The man’s face softened as he approached Stan and he extended his hand
+        to shake.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Welcome. I’m Eddie. Come in and grab a chair. Coffee is in the corner
+        on the table if you want some. We were just getting started.” Eddie
+        said, grabbing Stan’s hand.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Um. Thanks Eddie. I’m Stan.” He replied, returning the hand shake.
+      </p>
+      <br />
+      <p className="blog-p">
+        The two walked over to the circle of chairs and sat.
+      </p>
+      <br />
+      <p className="blog-p">
+        The group of people settled into their chairs as Stan glanced around at
+        all the faces, noticing the familiar shame in their eyes.
+      </p>
+      <br />
+      <p className="blog-p">
+        One older man sat alone by a window and stared aimlessly out.
+      </p>
+      <br />
+      <p className="blog-p">“Is he not part of this?” Stan asked nervously.</p>
+      <br />
+      <p className="blog-p">
+        “That’s Otis. Well, at least that’s what we call him.” Eddie began, “His
+        family drops him off to every meeting, but we’re still trying to
+        convince him to come join us. Otis, do you want to bring your chair over
+        to the circle and join in today?”
+      </p>
+      <br />
+      <p className="blog-p">
+        “Sittin’ in the mornin’ sun. I’ll be sittin’ when the evenin’ comes.
+        Watching the ships roll in. Then I watch ‘em roll away again.” Otis
+        replied.
+      </p>
+      <br />
+      <p className="blog-p">Eddie sighed.</p>
+      <br />
+      <p className="blog-p">
+        “Otis, it’s five in the evening. We’re way past the morning sun. And the
+        nearest body of water is over 100 miles away, so you aren’t watching any
+        ships. Your family cares about you. One day you should really join us.”
+      </p>
+      <br />
+      <p className="blog-p">Otis started to whistle.</p>
+      <br />
+      <p className="blog-p">
+        “Ok,” Eddie sighed again. “Let’s just get started.”
+      </p>
+      <br />
+      <p className="blog-p">
+        Eddie stood and motioned for the others to join him. One by one they
+        each stood.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Welcome to Lyric Speakers Anonymous. If you’ll all repeat after me. We
+        are not the songs. We have our own voices.” Eddie said.
+      </p>
+      <br />
+      <p className="blog-p">In unison the group echoed his words.</p>
+      <br />
+      <p className="blog-p">“We are not the songs. We have our own voices.”</p>
+      <br />
+      <p className="blog-p">
+        “Good. Good. Ok. Who wants to begin tonight?” Eddie said, glancing
+        around the group.
+      </p>
+      <br />
+      <p className="blog-p">
+        Everyone remained seated, nervously fidgeting as they waited for the
+        first brave volunteer to speak.
+      </p>
+      <br />
+      <p className="blog-p">After a moment, a young woman stood.</p>
+      <br />
+      <p className="blog-p">“Um, hi.” She said, wringing her hands.</p>
+      <br />
+      <p className="blog-p">
+        “My names Maggie and I’m addicted to speaking in lyrics.”
+      </p>
+      <br />
+      <p className="blog-p">“Hi Maggie.” Several other members said.</p>
+      <br />
+      <p className="blog-p">
+        “It’s been two days since I last used lyrics to speak instead of just
+        talking. It’s been tough. I know it upsets my family, and all my friends
+        have all but disowned me, but it’s just… it’s just…” her voice trailed
+        off.
+      </p>
+      <br />
+      <p className="blog-p">
+        “It’s ok Maggie. We all know it’s hard.” Eddie said, encouragement in
+        his voice.
+      </p>
+      <br />
+      <p className="blog-p">
+        “It’s just that, every morning I just look in the mirror and…” she
+        paused for a moment, then “tell the mirror what you know she’s heard
+        before. I don’t wanna be you. I don’t wanna be you. I don’t wanna be you
+        anymore.”
+      </p>
+      <br />
+      <p className="blog-p">Eddie drops his head, shaking it slowly.</p>
+      <br />
+      <p className="blog-p">
+        “You just did it again, didn’t you Maggie?” he said.
+      </p>
+      <br />
+      <p className="blog-p">Maggie shamefully nods and sits down silently.</p>
+      <br />
+      <p className="blog-p">
+        “It’s ok.” Eddie said, taking a deep breath to release his frustrations
+        with the moment.
+      </p>
+      <br />
+      <p className="blog-p">“Ok. Who is next?”</p>
+      <br />
+      <p className="blog-p">“I’ll go next doc.” A man said as he stood.</p>
+      <br />
+      <p className="blog-p">
+        “I’m not a doctor… you know what, never mind.” Eddie mumbled.
+      </p>
+      <br />
+      <p className="blog-p">
+        “My name is Chris and it’s been a few hours since I used lyrics instead
+        of my own words.” The man said.
+      </p>
+      <br />
+      <p className="blog-p">“Hi Chris.” The room replied.</p>
+      <br />
+      <p className="blog-p">Chris looks around solemnly.</p>
+      <br />
+      <p className="blog-p">
+        “Well I guess I was wrong. I just don’t belong. But then I’ve been there
+        before. Everything’s all right. I’ll just say goodnight and I’ll show my
+        self to the door.”
+      </p>
+      <br />
+      <p className="blog-p">“Chris, sit down.” Eddie grumbled.</p>
+      <br />
+      <p className="blog-p">
+        “I’m sorry doc. I’m trying, I really am.” Chris said as he lowered back
+        into his chair.
+      </p>
+      <br />
+      <p className="blog-p">Eddie rolled his eyes.</p>
+      <br />
+      <p className="blog-p">
+        “Listen, I know this is difficult, but every meeting can’t be this way.
+        You all have to try harder to…”
+      </p>
+      <br />
+      <p className="blog-p">
+        “Oooh AHHH AHH AHH AHH!” a seated man blurted out, causing everyone in
+        the room to tense.
+      </p>
+      <br />
+      <p className="blog-p">“LEE!” Eddie shouted.</p>
+      <br />
+      <p className="blog-p">
+        “Sorry. I can’t hold in the lyrics when I get nervous. It’s why my wife
+        left me… the divorce court was a nightmare.” The man who just blurted
+        out replied.
+      </p>
+      <br />
+      <p className="blog-p">
+        “The thrill is gone. The thrill is gone away.” Another member muttered
+        before clasping her hands over her mouth.
+      </p>
+      <br />
+      <p className="blog-p">
+        “No! We’re not having a chain reaction. Everyone just calm down. Take a
+        breath. We can get through this together. We have our own voices!” Eddie
+        said, teetering on the edge of his own frustrations.
+      </p>
+      <br />
+      <p className="blog-p">The room grew silent.</p>
+      <br />
+      <p className="blog-p">
+        “Um…” Stan said, standing nervously from his chair.
+      </p>
+      <br />
+      <p className="blog-p">
+        Eddie clasped his hands and looked up at Stan, eyes full of hope.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Yes, our newest member wants to share. Please do.”
+      </p>
+      <br />
+      <p className="blog-p">Stan breathed deep.</p>
+      <br />
+      <p className="blog-p">
+        “I’m Stan. This is my first time here, so I’m not really used to this,”
+        Stan said, head down, eyes on the floor in front of him.
+      </p>
+      <br />
+      <p className="blog-p">
+        “But,” he continued, “I think I understand this. It’s tough being like
+        this. Always struggling. Always fighting it.”
+      </p>
+      <br />
+      <p className="blog-p">
+        “Yes. Yes. Exactly Stan. You’ve got this. Please continue” Eddie beamed
+        with hope.
+      </p>
+      <br />
+      <p className="blog-p">
+        “The thing is…” Stan looked around, “You’ve got to lose yourself in the
+        music, the moment you own it. You better never let it go!”
+      </p>
+      <br />
+      <p className="blog-p">
+        “Of for f@#!’s sake!” Eddie threw his arms in the air.
+      </p>
+      <br />
+      <p className="blog-p">The room erupted in various lyrics.</p>
+      <br />
+      <p className="blog-p">“What looks so strong, so delicate!”</p>
+      <br />
+      <p className="blog-p">
+        “Some days I just wanna leave the negativity in my head, I just want
+        relief from my stress”
+      </p>
+      <br />
+      <p className="blog-p">“Because I’m happy”</p>
+      <br />
+      <p className="blog-p">“All my friends are heathens, take it slow”</p>
+      <br />
+      <p className="blog-p">“Hats off to the bull!”</p>
+      <br />
+      <p className="blog-p">“I’m gonna break my rusty cage and run”</p>
+      <br />
+      <p className="blog-p">“I want to paint it black”</p>
+      <br />
+      <p className="blog-p">
+        “I wish I would have met you, but now it’s a little late”
+      </p>
+      <br />
+      <p className="blog-p">“You gotta get that dirt off your shoulder”</p>
+      <br />
+      <p className="blog-p">
+        “Old sawbones got some age on him, that’s why you show respect”
+      </p>
+      <br />
+      <p className="blog-p">
+        “Something told me it was over, when I saw you and her talkin’ ”
+      </p>
+      <br />
+      <p className="blog-p">
+        “ENOUGH!” Eddie’s voice shattered the room, forcing silence where chaos
+        had just existed.
+      </p>
+      <br />
+      <p className="blog-p">
+        “You just don’t get it,” Eddie continued, falling to his knees “You just
+        don’t get it…”
+      </p>
+      <br />
+      <p className="blog-p">All eyes fell on Eddie.</p>
+      <br />
+      <p className="blog-p">
+        “Daddy didn’t give attention,” Eddie began, “to the fact that mommy
+        didn’t care”
+      </p>
+      <br />
+      <p className="blog-p">A gasp filled the room.</p>
+      <br />
+      <p className="blog-p">“You’re one of us?” an old voice asked.</p>
+      <br />
+      <p className="blog-p">
+        The room turned to see Otis standing, eyes glued to Eddie.
+      </p>
+      <br />
+      <p className="blog-p">Eddie nodded.</p>
+      <br />
+      <p className="blog-p">
+        “It’s ok son. We get it. Nobody knows you when you’re down and out.”
+      </p>
+      <br />
+      <p className="blog-p">
+        Stan looked around at the crowd of broken people.
+      </p>
+      <br />
+      <p className="blog-p">“Maybe next year” he shrugged.</p>
+      <br />
+      <br />
+      <p className="blog-p" style={{ fontWeight: "bold" }}>
+        I hope you enjoyed this bit of comedy. For anyone wondering what songs
+        were used in this short, in order of appearance they are:
+      </p>
+      <br />
+      <ul className="blog-ul" style={{ fontWeight: "bold" }}>
+        <li>Lose Yourself by Eminem</li>
+        <li>I write Sins Not Tragedies by Panic! At The Disco</li>
+        <li>(Sittin’ On) the Dock of the Bay by Otis Redding</li>
+        <li>Idontwannabeyouanymore by Billie Eilish</li>
+        <li>Friends in Low Places by Garth Brooks</li>
+        <li>Down with the Sickness by Disturbed</li>
+        <li>The Thrill is Gone by B.B. King</li>
+        <li>Lose Yourself by Eminem (for a second time)</li>
+        <li>Coming Undone by Korn</li>
+        <li>My Stress by NF</li>
+        <li>Happy by Pharrell Williams</li>
+        <li>Heathens by Twenty One Pilots</li>
+        <li>Rusty Cage by Soundgarden</li>
+        <li>Paint it Black by The Rolling Stones</li>
+        <li>Hey Man Nice Shot by Filter</li>
+        <li>Dirt Off Your Shoulder by Jay-Z</li>
+        <li>Reckoning Lament by Lincoln Durham</li>
+        <li>I’d Rather Go Blind by Etta James</li>
+        <li>Jeremy by Pearl Jam</li>
+        <li>
+          Nobody Knows You (When You’re Down and Out) by Ottis Redding
+        </li>
+      </ul>
+      <br />
+    </>
+  );
+}
+
+export default WritingPrompt6;
