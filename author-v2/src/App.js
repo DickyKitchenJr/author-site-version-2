99284c5f461cae6c356654188a50947f4c602e14
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-09 10:45:32 -0500

    Update App.js

--- author-v2/src/App.js
+++ author-v2/src/App.js
@@ -1,28 +1,28 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Header from './Components/Header';
-import Copyright from './Components/Copyright';
-import LandingPage from './Components/LandingPage';
-import About from './Components/About';
-import KidFriendly from './Components/KidFriendly';
-import Thrillers from './Components/Thrillers';
-import BlogSection from './Components/BlogSection';
-import BookInd from './Components/BookInd';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Copyright from "./Components/Copyright";
+import LandingPage from "./Components/LandingPage";
+import About from "./Components/About";
+import KidFriendly from "./Components/KidFriendly";
+import Thrillers from "./Components/Thrillers";
+import BlogSection from "./Components/BlogSection";
+import BookInd from "./Components/BookInd";
 
 function App() {
   return (
     <>
-    <Header />
+      <Header />
       <Routes>
-        <Route path='/' element = {<LandingPage />} />
-        <Route path='/about' element= {<About />} />
-        <Route path='/blogs/*' element={<BlogSection />} />
-        <Route path='/thrillers' element={<Thrillers />} />
-        <Route path='/kidfriendly' element= {<KidFriendly />} />
-        <Route path='/bookind' element={<BookInd />} />
-        <Route path='/*' element = {<LandingPage/>} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs/*" element={<BlogSection />} />
+        <Route path="/thrillers" element={<Thrillers />} />
+        <Route path="/kidfriendly" element={<KidFriendly />} />
+        <Route path="/bookind" element={<BookInd />} />
+        <Route path="/*" element={<LandingPage />} />
       </Routes>
-    <Copyright />
+      <Copyright />
     </>
   );
 }
