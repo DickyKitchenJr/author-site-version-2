0a35f82a16522a9371e46475fc0e4a2a92b793ca
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-13 10:12:49 -0500

    Create WritingPrompt8.js

---
+++ author-v2/src/Blogs/WritingPrompt8.js
@@ -0,0 +1,270 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import WriteSomething from "../Images/write-something.webp";
+import Cuffs from '../Images/handcuffs.webp';
+
+function WritingPrompt8() {
+  return (
+    <>
+      <br />
+      <NavLink className="blog-nav-link" to="/blogs">
+        ← back
+      </NavLink>
+
+      <div className="blog-header">
+        <h2 className="blog-h2">BookInd Writing Prompt</h2>
+        <h3 className="blog-h3">Prompt from 11/02/2022</h3>
+      </div>
+
+      <figure className="blog-figure">
+        <img
+          className="blog-img"
+          src={WriteSomething}
+          alt="typewriter with 'write something' on the paper in it"
+        />
+      </figure>
+
+      <p className="blog-p">
+        Every week a new writing prompt is added to the BookInd community. The
+        rules are simple - use the prompt or it's essence to create a short
+        piece of writing. The following is one such prompt and my subsequent use
+        of the prompt:
+      </p>
+      <br />
+      <p className="blog-p writing-prompt">Writing Prompt -</p>
+
+      <figure className="blog-figure">
+        <img className="blog-img" src={Cuffs} alt="a pair of handcuffs" />
+        <figcaption className="blog-fig-cap">Visual Writing Prompt</figcaption>
+      </figure>
+
+      <p className="blog-p">Darkness turned to light.</p>
+      <br />
+      <p className="blog-p">Blurred images began to form, and with it, pain.</p>
+      <br />
+      <p className="blog-p">
+        His head throbbed and swam with confusion as he tried to place his
+        surroundings.
+      </p>
+      <br />
+      <p className="blog-p">
+        The last thing he remembered was going for a walk to get to know his new
+        environment.
+      </p>
+      <br />
+      <p className="blog-p">He remembered looking down a dark alley…</p>
+      <br />
+      <p className="blog-p">But then what?</p>
+      <br />
+      <p className="blog-p">
+        He vaguely remembered a loud noise and a sting at the base of his skull
+        before the world went black.
+      </p>
+      <br />
+      <p className="blog-p">
+        Struggling to focus, his surroundings began to dance into focus.
+      </p>
+      <br />
+      <p className="blog-p">
+        He found himself sitting in a wooden chair, and tried to push himself up
+        only to be pulled back down.
+      </p>
+      <br />
+      <p className="blog-p">
+        A pinch in his wrist caused him to look down, and he saw a pair of
+        handcuffs on each wrist, locking him to the arms of the wooden chair.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Good, sinner. You are awake. We may begin.” A voice spoke behind him.
+      </p>
+      <br />
+      <p className="blog-p">
+        Still groggy, he jumped at the sound of the voice.
+      </p>
+      <br />
+      <p className="blog-p">“Where am I?” he forced his voice to ask.</p>
+      <br />
+      <p className="blog-p">
+        “You are where all sinners eventually find themselves. You are at the
+        gates of hell. But fear not. I will purge the sin from you and send you
+        to our lord clean and reborn.” The voice spoke from over his shoulder.
+      </p>
+      <br />
+      <p className="blog-p">
+        With the drama of a Shakespearean actor, a man walked from behind him.
+        Each foot planting with a slow methodical intention dripping with
+        pseudo-showmanship.
+      </p>
+      <br />
+      <p className="blog-p">
+        He looked over the man, noting the stained robe and askew clerical
+        collar he wore.
+      </p>
+      <br />
+      <p className="blog-p">
+        His shoulders drooped and he shook the rest of the cobwebs from his
+        head.
+      </p>
+      <br />
+      <p className="blog-p">
+        With new clarity, he pulled at his bindings, testing the strength of the
+        handcuffs latching him to the chair.
+      </p>
+      <br />
+      <p className="blog-p">
+        “There is no use struggling child. You will be delivered from these
+        mortal coils. The fire will cleanse you.” His captor said.
+      </p>
+      <br />
+      <p className="blog-p">“Fire?” he asked, tensing in the chair.</p>
+      <br />
+      <p className="blog-p">
+        The death cleric continued his exaggerated walk to a sheet covered table
+        and yanked the sheet off, revealing a propane torch.
+      </p>
+      <br />
+      <p className="blog-p">
+        His blood pumped faster at the sight of the torch and his eyes darted
+        around the room looking for any source of salvation from what was about
+        to become a very painful experience. That’s when he noticed it.
+      </p>
+      <br />
+      <p className="blog-p">
+        A chuckle escaped his mouth, and the death cleric’s dramatic performance
+        faltered as a look of confusion crossed his face.
+      </p>
+      <br />
+      <p className="blog-p">
+        “You find this funny, sinner?” the death cleric asked.
+      </p>
+      <br />
+      <p className="blog-p">
+        “It’s just that…” he began, then dropped his head and began to whisper
+        mumbles.
+      </p>
+      <br />
+      <p className="blog-p">
+        The death cleric drew closer, leaning down to put his ears closer to his
+        captive’s voice.
+      </p>
+      <br />
+      <p className="blog-p">
+        “What sinner? Speak up.” The death cleric insisted.
+      </p>
+      <br />
+      <p className="blog-p">
+        He raised his head, becoming eye to eye with the death cleric, and a
+        grin spread over his face.
+      </p>
+      <br />
+      <p className="blog-p">
+        “I said, you forgot to shackle my feet, amateur.”
+      </p>
+      <br />
+      <p className="blog-p">
+        With an explosion of movement he pushed off both feet, launching himself
+        into the air and thrusting his knees into the death cleric’s jaw.
+      </p>
+      <br />
+      <p className="blog-p">
+        The surprise impact caused the death cleric to fly backwards, his feet
+        losing purchase with the floor, and he came crashing down, all his air
+        evacuating his lungs with the impact.
+      </p>
+      <br />
+      <p className="blog-p">
+        Crashing down from his attack, the chair shattered and quickly freed the
+        wooden chunks from the ends of the handcuffs.
+      </p>
+      <br />
+      <p className="blog-p">
+        Standing from the loose pile of broken and splintered wood, he stood,
+        empty ends of the handcuffs swinging loosely from each wrist.
+      </p>
+      <br />
+      <p className="blog-p">
+        Pain radiated out from his wrist as blood trickled down, and yet he
+        smiled.
+      </p>
+      <br />
+      <p className="blog-p">
+        “What is it with the religious spin and fire? Do all of you nut jobs
+        subscribe to the same magazines or something?” he said grinning down on
+        the death cleric.
+      </p>
+      <br />
+      <p className="blog-p">
+        He took a few steps back, giving the death cleric room to stand, then
+        placed the open end of each handcuff in his hands. Turning what was once
+        his restraints into a makeshift pair of brass knuckles.
+      </p>
+      <br />
+      <p className="blog-p">
+        Confusion melted to rage, and the death cleric shot up from the ground
+        and scrambled for the torch on the table.
+      </p>
+      <br />
+      <p className="blog-p">
+        A metal laden punch came down on the death cleric’s knuckles as he
+        reached out to grasp the torch.
+      </p>
+      <br />
+      <p className="blog-p">
+        “Nope. Try again.” he derided the death cleric, watching as the would-be
+        killer withdrew his hand in pain.
+      </p>
+      <br />
+      <p className="blog-p">
+        The death cleric lunged at him, and was rewarded with several metallic
+        punches to the face.
+      </p>
+      <br />
+      <p className="blog-p">
+        The handcuffs tore chunks from the death clerics face with each blow,
+        and forced him to the ground.
+      </p>
+      <br />
+      <p className="blog-p">
+        Looking up from the ground, the death cleric wondered how things had
+        gone so wrong with his plan. He had always made a point to pick the most
+        unremarkable victims possible, knowing that they wouldn’t pose a threat
+        to him, and this man standing above him was no different.
+      </p>
+      <br />
+      <p className="blog-p">
+        There was nothing at all that would have indicated this man would have
+        been capable of besting him, he was just so… average.
+      </p>
+      <br />
+      <p className="blog-p">
+        The Average Man stood above the death cleric, and pushed the empty ends
+        of the handcuffs, opening each to a hook.
+      </p>
+      <br />
+      <p className="blog-p">
+        “There’s a ‘teach a man to fish’ joke in here somewhere.” He said
+        smiling down.
+      </p>
+      <br />
+      <p className="blog-p">
+        Then he drove the hooks down into the beaten death cleric’s neck and
+        yanked back until the ripped free.
+      </p>
+      <br />
+      <p className="blog-p">
+        Dripping with blood not his own, he looked around at his surroundings as
+        his victim lay bleeding out at his feet.
+      </p>
+      <br />
+      <p className="blog-p">
+        Raising his hands, he stared at the dripping handcuffs.
+      </p>
+      <br />
+      <p className="blog-p">“Now how am I going to get these things off?”</p>
+      <br />
+    </>
+  );
+}
+
+export default WritingPrompt8
