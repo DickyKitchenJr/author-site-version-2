9dcb5ab37976af100b6dc753b66bb6ba1557e0af
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-02-16 14:10:47 -0500

    Update App.js
    beginning steps of improving page load time

--- author-v2/src/App.js
+++ author-v2/src/App.js
@@ -1,14 +1,18 @@
+import React, { lazy, Suspense } from "react";
 import "./App.css";
 import ScrollToTop from "./Helpers/ScrollToTop";
 import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Copyright from "./Components/Copyright";
 import LandingPage from "./Components/LandingPage";
-import About from "./Components/About";
+// import About from "./Components/About";
 import KidFriendly from "./Components/KidFriendly";
 import Thrillers from "./Components/Thrillers";
-import BlogSection from "./Components/BlogSection";
+// import BlogSection from "./Components/BlogSection";
 import BookInd from "./Components/BookInd";
+
+const About = lazy(() => import('./Components/About'));
+const BlogSection = lazy(() => import("./Components/BlogSection"));
 
 function App() {
   return (
@@ -17,8 +21,20 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blogs/*" element={<BlogSection />} />
+        <Route 
+        path="/about" 
+        element={ <Suspense fallback={ <h1 style={{ padding: "10rem" }}>Fetching the awesomeness...</h1>}>
+          <About />
+          </Suspense>}/>
+        <Route
+          path="/blogs/*"
+          element={
+            <Suspense fallback={<h1 style={{ padding: "10rem" }}>Fetching the awesomeness...</h1>}>
+              <BlogSection />
+            </Suspense>
+          }
+        />
+
         <Route path="/thrillers" element={<Thrillers />} />
         <Route path="/kidfriendly" element={<KidFriendly />} />
         <Route path="/bookind" element={<BookInd />} />
